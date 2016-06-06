500b64b00ffaf038fd733861da2d42c1ed3807a3
ovrmrw <t.ngc.0403@gmail.com>
2016-06-06 15:19:54 +0900

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # angular2-testing-lite-5min-sample
 angular2-testing-lite with 5 MIN QUICKSTART(Angular2 official tutorial)
+
+---
+
+angular2-testing-lite を使ってテストを書くAngular2のミニマムなサンプル。
+
+コードを変更する度にブラウザもコンソールも更新され、テストが失敗すればすぐにわかります。
+
+###Setup
+```
+$ npm install
+```
+
+###Run
+```
+$ npm start
+```
---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {AppComponent2} from './app2.component'; // async/await sample.
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>My First Angular 2 App</h1>
+    <p>My Test</p>
+    <my-app2></my-app2>
+  `,
+  directives: [AppComponent2]
+})
+export class AppComponent {
+  getNumber() {
+    return 100;
+  }
+}
